e61e2525015976afc429b36547c7bbc2b8cd755d
GhosTHaise <Ghostrex2@gmail.com>
2023-01-07 19:49:10 +0300

    Finish to build fecth api librairie

--- api/src/lib.rs
+++ api/src/lib.rs
@@ -1,18 +1,42 @@
-use reqwest;
+use std::{string, collections::HashMap};
+
+use reqwest::{Url, Response};
 use serde::{Serialize,Deserialize};
 use serde_derive::{Serialize, Deserialize};
-struct Api{
-
+use exitfailure::{self, ExitFailure};
+struct Api<'a>{
+    pub url : &'a String
 }
 
 #[derive(Serialize,Deserialize)]
 struct Payload {
-    
+    bot : String
 }
 
-impl Api {
-    pub fn fetch() ->  () {
-        
-        todo!();
+impl Api<'_> {
+    pub fn new(url : &String) -> Api {
+        Api{
+            url
+        }
     }
+    #[cfg(test)]
+    pub async fn fetch(&self,prompt : String) ->  Result<Payload,ExitFailure> {
+        let url  = String::from(self.url);
+        let url = Url::parse(&url)?;
+        //body
+        let mut body_map_json = HashMap::new();
+        body_map_json.insert("prompt", prompt);
+        //fetch api
+        let client = reqwest::Client::new();
+        let response  = client
+                    .post(url)
+                    .json(&body_map_json)
+                    .send()
+                    .await?
+                    .json::<Payload>()
+                    .await?
+                    ;
+        Ok(response)    
+    }
+        
 }
