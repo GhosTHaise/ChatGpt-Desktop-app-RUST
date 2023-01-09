3e60a74ea4d27e0df5174dfb8e03c47cd0c3228b
GhosTHaise <Ghostrex2@gmail.com>
2023-01-09 19:54:31 +0300

    create render tool and function

--- headlines/src/headlines.rs
+++ headlines/src/headlines.rs
@@ -1,5 +1,5 @@
 use confy;
-use eframe::egui::{Context, TopBottomPanel,TextEdit, output, self};
+use eframe::{egui::{Context, TopBottomPanel,TextEdit, output, self, TextStyle, Label, RichText}, epaint::FontId};
 use serde::{Serialize,Deserialize};
 
 #[derive(Serialize,Deserialize)]
@@ -37,12 +37,41 @@ fn clear_intput(mut value : &mut String){
     value.clear();
 }
 
+fn loader(curentText : &str,label : egui::Label){
+    let mut i = 0;
+    loop{
+        if(i != 4){
+            //label.set_text(format!("{}.",curentText));
+        }else{
+            i = 0;
+        }
+        std::thread::sleep(std::time::Duration::from_secs(1));
+        i = i + 1;
+    }
+}
+pub fn render_new_message(is_bot : bool,content : String,ui : &mut eframe::egui::Ui){
+    let mut label = Label::new(RichText::new(content).text_style(egui::TextStyle::Body));
+    ui.add(label);
+}
+
 pub fn render_message_bottom(ctx : &Context, content : &mut String)-> () {
     TopBottomPanel::bottom("message").show(ctx , |ui|{
-        
+        let mut style = (*ctx.style()).clone();
+        //Adjust global font size
+        style.text_styles = [
+        (TextStyle::Heading,FontId::new(32.0, eframe::epaint::FontFamily::Proportional)), 
+        (TextStyle::Body,FontId::new(18.0, eframe::epaint::FontFamily::Proportional)),
+        (TextStyle::Monospace,FontId::new(14.0, eframe::epaint::FontFamily::Proportional)),
+        (TextStyle::Button,FontId::new(14.0, eframe::epaint::FontFamily::Proportional)),
+        (TextStyle::Small,FontId::new(10.0, eframe::epaint::FontFamily::Proportional))   
+        ].into();
+        ctx.set_style(style);
         ui.horizontal(|ui|{
             ui.set_height(50.);
-            let mess = ui.add_sized(ui.available_size(),TextEdit::singleline(content ).hint_text("Ask GhosT ...").text_style(egui::TextStyle::Body));
+            let mess = ui.add_sized(
+                    ui.available_size(),
+                      TextEdit::singleline(content ).hint_text("Ask GhosT ...").font(egui::TextStyle::Body)
+                    );
             if mess.changed(){
                 //println!("{:?}",mess);
             }
